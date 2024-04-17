52efeaec60363d01ced8d6f83f9b65c40dd91ac7
cesarealmendarez <cesarealmendarez@gmail.com>
2024-04-16 22:28:31 -0700

    initialization

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const business = {
+    name: "Joy Thai",
+    address: "1900 W Valley Blvd Alhambra, CA 91803",
+    phone_number: "+1 (323) 736-1188",
+    description: "Joy Thai is a chic Thai eatery merging tradition with contemporary flair. Step into a space adorned with subtle Thai motifs and ambient lighting. Choose from a carefully curated and inspired menu celebrating Thailand's culinary heritage, from fresh spring rolls to aromatic yellow curry.",
+    hours: {
+        monday: "closed",
+        tuesday: "11am-3pm, 5pm-9pm",
+        wednesday: "11am-3pm, 5pm-9pm",
+        thursday: "11am-3pm, 5pm-9pm",
+        friday: "11pm-3pm, 5pm-9pm",
+        saturday: "5pm-9pm",
+        sunday: "11am-3pm, 5-9pm"
+    }
+};
